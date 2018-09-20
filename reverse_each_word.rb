cf306ed93987bc534cfa830a155df51d4e361ee6
carly <carly@noukod.com>
2018-09-20 18:01:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@
 def reverse_each_word phrase
   arrayPhrase = phrase.split(' ')
 
-  reversedPhrase = arrayPhrase.collect {|word| word.reverse}
+  reversedPhrase = arrayPhrase.map {|word| word.reverse}
 
   reversedPhrase.join(' ')
 end
