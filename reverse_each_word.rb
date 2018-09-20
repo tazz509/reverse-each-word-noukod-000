c26dd8ceba85f1a8c62278c3223cb0df70369634
carly <carly@noukod.com>
2018-09-20 17:55:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 welcome = "Hello there, and how are you?"
 array.new = welcome.split (",")
 def reverse_each_word array
-  
+
   array.each do |word|
     array.reverse
     array.split
-end  
+end
 end
